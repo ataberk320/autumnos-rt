d048259becad1874d0287f78309e86b11a8e11a2
Autixberk <atatv183@gmail.com>
2026-05-15 01:07:51 +0300

    Update main.h

--- src/main.h
+++ src/main.h
@@ -10,11 +10,5 @@ void a_kpanic(const char *reason);
 void entry(void *par);
 void corethr(void);
 
-//task.c
-rt_thread_t acrtask(const char *name, void (*entry) (void* parameter), uint32_t stack_size, uint8_t priority);
-
-//mem.c
-void* atm_malloc(uint32_t size);
-void afree(void *ptr, uint32_t size);
 
 #endif
