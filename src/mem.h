4c12d5cf96566ea1f10f14c7bf8940ce02c448cb
Autixberk <atatv183@gmail.com>
2026-05-15 10:27:59 +0300

    Delete src/mem.h

--- src/mem.h
+++
@@ -1,8 +0,0 @@
-#ifndef MEM_H
-#define MEM_H
-
-void* atm_malloc(uint32_t size);
-void afree(void *ptr, uint32_t size);
-rt_thread_t acrtask(const char *name, void (*entry) (void* parameter), uint32_t stack_size, uint8_t priority);
-
-#endif
