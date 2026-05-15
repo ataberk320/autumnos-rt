94949a59519a731664f7418d79ea1814a7c53699
Autixberk <atatv183@gmail.com>
2026-05-15 10:25:15 +0300

    Add files via upload

---
+++ src/cdictheader/main.h
@@ -0,0 +1,26 @@
+#ifndef MAINFUNC_H
+#define MAINFUNC_H
+
+#include <rtthread.h>
+
+//panic.c
+void a_kpanic(const char *reason);
+
+//acore.c
+void entry(void *par);
+void corethr(void);
+
+//task.c
+rt_thread_t acrtask(const char *name, void (*entry) (void* parameter), uint32_t stack_size, uint8_t priority);
+
+//mem.c
+void* atm_malloc(uint32_t size);
+void afree(void *ptr, uint32_t size);
+
+//AutumnIO.c
+int aopen(const char *path, int flags);
+int aread(int fd, void *buf, uint32_t len);
+int awrt(int fd, const void *buf, uint32_t len);
+void aclose(int fd);
+
+#endif
